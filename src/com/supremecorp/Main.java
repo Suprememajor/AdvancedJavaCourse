6b5138bb639f2ef297d6cd635da278fdf0b042d3
Suprememajor <ndipnobertetta@gmail.com>
2022-01-25 15:03:59 +0100

    enumerations

--- src/com/supremecorp/Main.java
+++ src/com/supremecorp/Main.java
@@ -4,5 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Color c1 = Color.RED;
+
+        System.out.println("Red Enum name: " + c1.name());
+        System.out.println("Red Enum value: " + c1.getValue());
+
+        for (Color color: Color.values()) {
+            System.out.println("Enum value: " + color.getValue());
+        }
+    }
+}
+
+enum Color {
+    RED("red"), GREEN("green"), BLUE("blue");
+
+    private String value;
+
+    Color(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
     }
 }
